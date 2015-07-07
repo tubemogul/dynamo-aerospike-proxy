44daeff5dd1dbb10a799bbc49a692de2bd2849df
Pierre GRANDIN <pierre.grandin@tubemogul.com>
2015-07-07 03:33:28 -0400

    yFirst functional POC

--- main.go
+++ main.go
@@ -2,33 +2,51 @@ package main
 
 import (
 	"fmt"
+	. "github.com/aerospike/aerospike-client-go"
+	"github.com/gorilla/mux"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/awsutil"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 func main() {
-	key := "XXX"
-	svc := dynamodb.New(&aws.Config{Region: "us-west-1", LogLevel: 0})
-	fmt.Printf("%+v\n", svc)
-	result, err := svc.ListTables(&dynamodb.ListTablesInput{})
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/get/{user_key}", FetchFromAS)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func FetchFromAS(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	user_key := vars["user_key"]
+	client, err := NewClient(os.Args[1], 3000)
+	key, err := NewKey("memory", "demoset", user_key)
 	if err != nil {
-		log.Println(err)
-		return
+		log.Fatal(err)
 	}
-
-	log.Println("Tables:")
-	for _, table := range result.TableNames {
-		log.Println(*table)
+	rec, err := client.Get(nil, key)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		if rec != nil {
+			w.Header().Set("X-Cache", "HIT")
+			fmt.Fprintln(w, "got:", rec)
+		} else {
+			w.Header().Set("X-Cache", "MISS")
+			getFromDynamo(user_key)
+		}
 	}
+}
 
+func getFromDynamo(user_key string) {
+	svc := dynamodb.New(&aws.Config{Region: "us-west-1", LogLevel: 0})
 	params := &dynamodb.GetItemInput{
 		Key: map[string]*dynamodb.AttributeValue{ // Required
 			"user_key": {
-				S: aws.String(key),
+				S: aws.String(user_key),
 			},
 		},
 		TableName: aws.String("user_profiles"), // Required
@@ -36,10 +54,8 @@ func main() {
 			aws.String("user_key"),     // Required
 			aws.String("version"),      // Required
 			aws.String("user_profile"), // Required
-			// More values...
 		},
 	}
-	fmt.Println(awsutil.StringValue(params))
 	resp, err := svc.GetItem(params)
 	if err != nil {
 		if awsErr, ok := err.(awserr.Error); ok {
@@ -54,7 +70,15 @@ func main() {
 			// error which satisfies the awserr.Error interface.
 			fmt.Println(err.Error())
 		}
+	} else {
+		if resp.Item["user_profile"] != nil {
+			client, err := NewClient(os.Args[1], 3000)
+			key, err := NewKey("memory", "demoset", user_key)
+			if err != nil {
+				log.Fatal(err)
+			}
+			bin1 := NewBin("bin1", resp.Item["user_profile"].GoString())
+			client.PutBins(nil, key, bin1)
+		}
 	}
-	fmt.Printf("%+v\n", resp)
-
 }
